533e9ac7e8526d220fd4194cab3fcaefcb2f0c12
Fabrizio <fabrizio.cocco@supsign.ch>
2020-09-09 21:30:00 +0200

    no message

--- src/AlltronPriceImport.php
+++ src/AlltronPriceImport.php
@@ -9,18 +9,45 @@ class AlltronPriceImport
 // Noch Auslagern in .env und Config
    
 
-
+  protected
+  $ftp;
 
     public function __construct() {
-           
+      
            $host = 'ftp.competec.ch';
            $login = 'A273237';
            $password = '4KbddyPN';
        
-        $ftp = new \FtpClient\FtpClient();
-        $ftp->connect($host);
-        $ftp->login($login, $password);
+        $this->ftp = new \FtpClient\FtpClient();
+        $this->ftp->connect($host);
+        $this->ftp->login($login, $password);
+        // $this->ftp->pasv(true);
 
-        return $ftp;
+
+        return $this;
     }
+
+
+public function setFile($file){
+    // $this->ftp->
+    // return $this;
+}
+
+
+    public function importPrices(){
+        // foreach($this as){
+        //     $this->importPrice();
+        // }
+    }
+
+    protected function importPrice(){
+
+    }
+
+    public function test(){
+        return $this->ftp->count();
+    }
+
+
+ 
 }
