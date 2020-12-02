146230e0faa8b87e7c255de1f082fd8d939f9d0b
Florian Ratz <florian.ratz@supsign.ch>
2020-12-02 11:33:58 +0100

    use our own ftp connector

--- src/AlltronFTP.php
+++ src/AlltronFTP.php
@@ -2,50 +2,12 @@
 
 namespace Supsign\Alltron;
 
-class AlltronFTP
-{
-    protected
-        $ftp = null;
+use Supsign\LaravelFtpConnector\FtpConnector;
 
+class AlltronFTP extends FtpConnector
+{
     public function __construct() 
     {
-        $this->ftp = new \FtpClient\FtpClient();
-        $this->ftp->connect(env('ALLTRON_FTP_HOST'));
-        $this->ftp->login(env('ALLTRON_FTP_LOGIN'), env('ALLTRON_FTP_PASSWORD'));
-
-        return $this;
-    }
-
-    protected function download($file)
-    {
-        file_put_contents(storage_path().'/'.$file, $this->ftp->getContent($file));
-
-        return $this;
-    }
-
-    public function downloadPriceData()
-    {
-        return $this->download('PreisdatenV2.XML');
-    }
-
-    public function downloadProductData()
-    {
-        return $this->download('ArtikeldatenV2.xml');
-    }
-
-
-    protected function importPrices()
-    {
-
-    }
-
-    protected function importPrice()
-    {
-
-    }
-
-    public function test() 
-    {
-
+        return parent::__construct('ALLTRON');
     }
 }
